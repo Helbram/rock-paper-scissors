9ff2fa55a7e6f292b9826c3e0a157b4da6f22af8
Nathan Barrett <natewbarrett@gmail.com>
2025-02-16 18:13:55 +0000

    Created getComputerChoice function that picks a random number between 0 and 2 and returns rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+// When the user selects an option (rock, paper scissors)
+// Store in a string variable
+// Compare user input to possible options
+// Output if user won, lost, or drew
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "rock";
+    } else if (choice == 1) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
